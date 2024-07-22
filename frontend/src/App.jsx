76ccc6636b737347b1da324675fb1539446d4547
Nguyen Tien Hau <nguyentienhau1862@gmail.com>
2024-07-22 18:03:43 +0700

    Create app routers and homepage

---
+++ frontend/src/App.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+import { NavMenu } from "@shopify/app-bridge-react";
+
+export default function App() {
+	return <NavMenu>
+		<a href="/" rel="home" />
+		{/* <a href="/pagename">{t("NavigationMenu.pageName")}</a> */}
+	</NavMenu>;
+}
